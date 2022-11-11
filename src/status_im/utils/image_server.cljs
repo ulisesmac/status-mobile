7248f743823fb125691361846b6558974e39f5c2
siddarthkay <siddarthkay@gmail.com>
2022-11-11 16:08:15 +0530

    dummy component to test new QR codes

--- src/status_im/utils/image_server.cljs
+++ src/status_im/utils/image_server.cljs
@@ -1,5 +1,7 @@
 (ns status-im.utils.image-server
-  (:require [quo.design-system.colors :as colors]))
+  (:require [quo.design-system.colors :as colors]
+            [status-im.utils.config :as config]
+            [taoensso.timbre :as log]))
 
 (def ^:const image-server-uri-prefix "https://localhost:")
 (def ^:const identicons-action "/messages/identicons")
@@ -18,7 +20,19 @@
   (str image-server-uri-prefix port identicons-action "?publicKey=" public-key "&theme=" (current-theme) "&clock=" (timestamp) "&addRing=1"))
 
 (defn get-account-image-uri [port public-key image-name key-uid]
-  (str image-server-uri-prefix port account-images-action "?publicKey=" public-key "&keyUid=" key-uid "&imageName=" image-name "&theme=" (current-theme) "&clock=" (timestamp) "&addRing=1"))
+  (let [base (str image-server-uri-prefix port account-images-action "?publicKey=" public-key "&keyUid=" key-uid "&imageName=" image-name "&theme=" (current-theme) "&clock=" (timestamp))]
+    (log/debug "get-account-image-uri===>" base)
+    (cond-> base
+      @config/new-ui-enabled? (str "&addRing=1"))))
 
 (defn get-contact-image-uri [port public-key image-name clock]
-  (str image-server-uri-prefix port contact-images-action "?publicKey=" public-key "&imageName=" image-name "&theme=" (current-theme) "&clock=" clock "&addRing=1"))
+  (let [base (str image-server-uri-prefix port contact-images-action "?publicKey=" public-key "&imageName=" image-name "&theme=" (current-theme) "&clock=" clock)]
+
+    (cond-> base
+      @config/new-ui-enabled? (str "&addRing=1"))))
+
+(defn get-dummy-qr-uri [port public-key image-name key-uid]
+  (let [base (str image-server-uri-prefix port account-images-action "?publicKey=" public-key "&keyUid=" key-uid "&imageName=" image-name "&theme=" (current-theme) "&clock=" (timestamp))]
+
+    (cond-> base
+            @config/new-ui-enabled? (str "&addRing=1"))))
