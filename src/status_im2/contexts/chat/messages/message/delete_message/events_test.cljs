66b5794560f6dec1e0a6ea680634aecf69f75b53
yqrashawn <namy.19@gmail.com>
2022-12-21 14:20:58 +0800

    feat: undo delete with toast

--- src/status_im2/contexts/chat/messages/message/delete_message/events_test.cljs
+++ src/status_im2/contexts/chat/messages/message/delete_message/events_test.cljs
@@ -18,8 +18,7 @@
             (is (true? (:deleted? result-message)))
             (is (= (:deleted-undoable-till result-message) 1001))))
         (testing "return nil if message not in db"
-          (is (= (delete-message/delete {:db {:messages []}} message 1000)
-                 nil)))))))
+          (is (= (delete-message/delete {:db {:messages []}} message 1000) nil)))))))
 
 (deftest undo-delete
   (let [db      {:messages {cid {mid {:id mid :whisper-timestamp 1}}}}
@@ -30,14 +29,11 @@
                                         [:messages cid mid]
                                         assoc
                                         :deleted?              true
-                                        :deleted-undoable-till
-                                        (+ (datetime/timestamp) 1000))
-              result-message (get-in (delete-message/undo {:db db} message)
-                                     [:db :messages cid mid])]
+                                        :deleted-undoable-till (+ (datetime/timestamp) 1000))
+              result-message (get-in (delete-message/undo {:db db} message) [:db :messages cid mid])]
           (is (= (:id result-message) mid))
           (is (nil? (:deleted? result-message)))
           (is (nil? (:deleted-undoable-till result-message)))))
-
       (testing "remain deleted when undo after timelimit"
         (let [db             (update-in db
                                         [:messages cid mid]
@@ -48,10 +44,8 @@
           (is (= (:id result-message) mid))
           (is (nil? (:deleted-undoable-till result-message)))
           (is (true? (:deleted? result-message)))))
-
       (testing "return nil if message not in db"
-        (is (= (delete-message/undo {:db {:messages []}} message)
-               nil))))))
+        (is (= (delete-message/undo {:db {:messages []}} message) nil))))))
 
 (deftest delete-and-send
   (let [db      {:messages {cid {mid {:id mid}}}}
@@ -64,31 +58,17 @@
               rpc-calls   (:json-rpc/call effects)]
           (is (= result-db expected-db))
           (is (= (count rpc-calls) 1))
-          (is (= (-> rpc-calls
-                     first
-                     :method)
-                 "wakuext_deleteMessageAndSend"))
-          (is (= (-> rpc-calls
-                     first
-                     :params
-                     count)
-                 1))
-          (is (= (-> rpc-calls
-                     first
-                     :params
-                     first)
-                 mid))))
+          (is (= (-> rpc-calls first :method) "wakuext_deleteMessageAndSend"))
+          (is (= (-> rpc-calls first :params count) 1))
+          (is (= (-> rpc-calls first :params first) mid))))
       (testing "clean undo timer"
         (let [expected-db {:messages {cid {mid {:id mid}}}}
               effects     (delete-message/delete-and-send
                            {:db (update-in db
-                                           [:messages cid mid
-                                            :deleted-undoable-till]
+                                           [:messages cid mid :deleted-undoable-till]
                                            (constantly (datetime/timestamp)))}
                            message)
               result-db   (:db effects)]
           (is (= result-db expected-db))))
       (testing "return nil if message not in db"
-        (is (= (delete-message/delete-and-send {:db {:messages []}}
-                                               message)
-               nil))))))
+        (is (= (delete-message/delete-and-send {:db {:messages []}} message) nil))))))
